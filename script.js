8e4d99b256a82c8da82ae3c5c2095ff7fcc2cd9a
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-18 16:13:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 fetch('https://tio-cooper.github.io/demo/demo.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
+
+
+const dbParam = JSON.stringify(fetch('https://tio-cooper.github.io/demo/demo.json'))
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  const myObj = JSON.parse(this.responseText);
+  let text = "<select>"
+  for (let x in myObj) {
+    text += "<option>" + myObj[x].name + "</option>";
+  }
+  text += "</select>"
+  document.getElementById("demo").innerHTML = text;
+}
+xmlhttp.open("POST", "json_demo_html_table.php");
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam);
