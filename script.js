5e3f2cef7c6e4ce64d5d3e54826fb3b8a6fd9cde
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-18 14:50:22 +0000

    90% Working

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-import data from 'https://raw.githubusercontent.com/tio-cooper/demo/refs/heads/main/demo.json' assert { type: 'json' };
+import data from 'https://tio-cooper.github.io/demo/demo.json' assert { type: 'json' };
 console.log(data);
