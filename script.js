89c087b3ceed5cab22f2a9f60289e9753ff2493c
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-19 13:34:06 +0000

    Multi Change

--- script.js
+++ script.js
@@ -2,24 +2,7 @@ fetch('https://tio-cooper.github.io/demo/demo.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
 
-
-const dbParam = JSON.stringify(fetch('https://tio-cooper.github.io/demo/demo.json'))
-const xmlhttp = new XMLHttpRequest();
-xmlhttp.onload = function() {
-  const myObj = JSON.parse(this.responseText);
-  let text = "<select>"
-  for (let x in myObj) {
-    text += "<option>" + myObj[x].name + "</option>";
-  }
-  text += "</select>"
-  document.getElementById("demo").innerHTML = text;
-}
-xmlhttp.open("POST", "json_demo_html_table.php");
-xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xmlhttp.send("x=" + dbParam);
-
-// compile the template
-var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>"); 
-// execute the compiled template and print the output to the console
-document.body.appendChild(template)
-console.log(template({ doesWhat: "rocks!" }));
+(function(){
+  var theScriptHTML = document.getElementById('techtag-content')
+  var theTemplate = Handlebars
+})
