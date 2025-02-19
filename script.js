3f0220f583e3eb782e1b69069976ac38cc1b3682
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-19 16:42:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-fetch('https://tio-cooper.github.io/demo/demo.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+// fetch('https://tio-cooper.github.io/demo/demo.json')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
 
-var theData = jQuery.getJSON('https://tio-cooper.github.io/demo/demo.json');
+var theData = jQuery.getJSON('https://tio-cooper.github.io/demo/demo.json', function(data));
+console.log("V1");
 console.log(theData);
+
+let theDatav2;
+fetch('https://tio-cooper.github.io/demo/demo.json').then(
+  function(u){return u.json();}
+).then(
+  function(json){
+    theDatav2 = json
+  }
+)
+
+console.log("V2");
+console.log(theDatav2)
+
 
 (function(){
   var theScriptHTML = document.getElementById('demo')
