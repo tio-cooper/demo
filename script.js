f9d4810ee361adb68f24795330aeec9e0e3c090c
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-19 16:19:16 +0000

    Multi Change

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 fetch('https://tio-cooper.github.io/demo/demo.json')
     .then((response) => response.json())
     .then((json) => console.log(json))
-    .then(contextThing = json);
+    .then(contextThing = (json));
+
+
+var theData = jQuery.getJSON('https://tio-cooper.github.io/demo/demo.json');
+console.log(theData);
 
 (function(){
   var theScriptHTML = document.getElementById('demo')
   var theTemplate = Handlebars.compile(theScriptHTML)
-  // var contextObj = {SiteCore: false};
   var compiledData = theTemplate(contextThing)
   document.getElementById('demo') = compiledData
 })
