2c71ad102ebe5720defbcb5d70ebfcd4fb838dc1
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-19 15:57:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ import Handlebars from "handlebars";
 
 fetch('https://tio-cooper.github.io/demo/demo.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => console.log(json))
+    .then(contextThing = response.json());
 
 (function(){
   var theScriptHTML = document.getElementById('techtag-content')
   var theTemplate = Handlebars.compile(theScriptHTML)
-  var contextObj = {SiteCore: false};
-  var compiledData = theTemplate(contextObj)
+  // var contextObj = {SiteCore: false};
+  var compiledData = theTemplate(contextThing)
   document.getElementById('teachtag-content') = compiledData
 })
