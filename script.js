50790af03343f9bccf4106cec472314af7bc921f
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-20 12:43:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let json_data = {};
+const json_data = {};
 (function(){
   const getData = () => {
     $.ajax({ url: 'https://tio-cooper.github.io/demo/demo.json'})
     .then(data => console.log('Initial', data))
-    .then(data => {json_data = data;})    
+    .then(data => {json_data = data})    
   }
   getData()
+
   console.log("Access", json_data)
   var theScriptHTML = document.getElementById('demo').innerHTML;
   var theTemplate = Handlebars.compile(theScriptHTML);
