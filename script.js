2e594ac494329245d529135a145e04ec28a9e621
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-20 09:42:39 +0000

    Multi Fix

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// fetch('https://tio-cooper.github.io/demo/demo.json')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
-
 var theData = jQuery.getJSON('https://tio-cooper.github.io/demo/demo.json');
-console.log(theData);
 
 (function(){
   var theScriptHTML = document.getElementById('demo').innerHTML;
   var theTemplate = Handlebars.compile(theScriptHTML);
   var contextObj = {SiteCore: {Google: "true"}};
   var compiledData = theTemplate(contextObj);
-
   document.getElementById('techtag').innerHTML = compiledData;
 }());
