5b89e60b0ba39dbc56d484cf42a27e5fcfe2afd2
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-20 09:33:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var theData = jQuery.getJSON('https://tio-cooper.github.io/demo/demo.json');
 console.log(theData);
 
 (function(){
-  var theScriptHTML = document.getElementById('techtag-content').innerHTML;
+  var theScriptHTML = document.getElementById('demo').innerHTML;
   var theTemplate = Handlebars.compile(theScriptHTML);
   var contextObj = {SiteCore: {Google: "true"}};
   var compiledData = theTemplate(contextObj);
