3f61a5d3f7ca6323e58a6fad80a3975fce025c10
tio-cooper <josh.cooper@turniton.co.uk>
2025-02-20 09:20:51 +0000

    Update script.js
    Fixing "theDate" to "theData"

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 //     .then((json) => console.log(json));
 
 var theData = jQuery.getJSON('https://tio-cooper.github.io/demo/demo.json');
-console.log(theDate);
+console.log(theData);
 
 (function(){
   var theScriptHTML = document.getElementById('techtag-content').innerHTML;
   var theTemplate = Handlebars.compile(theScriptHTML);
   var contextObj = {SiteCore: {Google: "true"}};
   var compiledData = theTemplate(contextObj);
-  
+
   document.getElementById('techtag') = compiledData;
 }());
